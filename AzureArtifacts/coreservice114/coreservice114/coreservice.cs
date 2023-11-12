2c93190314daaad0be0f6be453a4141a33a326db
VVHIT-DevOps <ashok@vvhitsolutions.com>
2023-11-12 12:49:51 +0000

    added artifacts project files

---
+++ AzureArtifacts/coreservice114/coreservice114/coreservice.cs
@@ -0,0 +1,10 @@
+﻿namespace coreservice114
+{
+    public class coreservice
+    {
+        public string Display()
+        {
+            return "this has core functions";
+        }
+    }
+}
---
+++ AzureArtifacts/testfeed1/testfeed1/Pages/Index.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using coreservice114;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace testfeed1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public string message { get; set; }
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            coreservice coreservice = new coreservice();
+            message = coreservice.Display();
+        }
+    }
+}
