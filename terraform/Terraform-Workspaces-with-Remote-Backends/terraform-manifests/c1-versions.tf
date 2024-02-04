d3168088e4e13f8173d253398500e6ff02661b89
VVHIT-DevOps <145626421+VVHIT-DevOps@users.noreply.github.com>
2024-02-04 10:36:42 +0530

    workspaces

---
+++ terraform/Terraform-Workspaces-with-Remote-Backends/terraform-manifests/c1-versions.tf
@@ -0,0 +1,36 @@
+# Terraform Block
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 2.0" 
+    }
+    random = {
+      source = "hashicorp/random"
+      version = ">= 3.0"
+    }
+  }
+# Terraform State Storage to Azure Storage Container
+  backend "azurerm" {
+    resource_group_name   = "terraform-store"
+    storage_account_name  = "terraformstore454"
+    container_name        = "statefilecontainer"
+    key                   = "cliworkspaces-terraform.tfstate"
+  }   
+}
+
+# Provider Block
+provider "azurerm" {
+ features {}          
+}
+
+# Random String Resource
+resource "random_string" "myrandom" {
+  length = 6
+  upper = false 
+  special = false
+  numeric = false   
+}
+
+
